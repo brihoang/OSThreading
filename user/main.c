96bc4673fefac83635e0b65fce0b5057b9b6ec77
Brian Hoang <bri.hoang@utexas.edu>
2014-11-25 01:32:06 -0600

    started tests

--- user/main.c
+++ user/main.c
@@ -1,6 +1,27 @@
 #include "libc.h"
+#include "threads.h"
 
 int main(){
 	puts("hellow!!!");
 	return 0;
+}
+
+void *print(void *args){
+	putdec(((long*)args)[0]);
+	return 0;
+}
+
+void test1(){
+	thread_t t1;
+	thread_t t2;
+
+	char* arg1[1];
+	char* arg2[1];
+
+	arg1[0] = "THREAD 1";
+	arg2[0] = "THREAD 2";
+	thread_create(&t1, &print, (void*)arg1);
+	thread_create(&t2, &print, (void*)arg2);
+	thread_exit();
+
 }
